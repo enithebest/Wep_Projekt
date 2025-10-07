adb1b7bb4643264b3f1da829e03ec9c499ca4e1b
enithebest <aenihasmegaj@gmail.com>
2025-10-07 10:32:35 +0100

    Initial setup with basic layout and two simple lists

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,20 @@
 <script>
-
+	let listA = [1, 2, 3];
+	let listB = [4];
 </script>
-<h1>
-    KANBAN BOARD
-</h1>
-<main class="w-full p-8 bg-gray-100 h-[400px] flex justify-between items-center">
-        <section class="h-[350px] w-[100px] bg-white border-2 border-black flex flex-col items-center justify-center">
-        </section>
-    <section  class="h-[350px] w-[100px] bg-white border-2 border-black flex flex-col items-center justify-center">
-    </section>
-    
+
+<h1 class="text-center text-xl font-semibold mb-4">Drag & Drop Practice</h1>
+
+<main class="flex justify-center gap-6 p-8 bg-gray-100 h-[400px]">
+	<section class="h-[350px] w-[100px] bg-white border-2 border-black flex flex-col items-center justify-center">
+		{#each listA as item}
+			<article class="p-3 bg-gray-400 rounded-md mb-2">{item}</article>
+		{/each}
+	</section>
+
+	<section class="h-[350px] w-[100px] bg-white border-2 border-black flex flex-col items-center justify-center">
+		{#each listB as item}
+			<article class="p-3 bg-gray-400 rounded-md mb-2">{item}</article>
+		{/each}
+	</section>
 </main>
