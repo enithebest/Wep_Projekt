b5c9492f7612d7c53dfefe428044388f953d1b4b
enithebest <aenihasmegaj@gmail.com>
2025-10-07 10:17:14 +0100

    sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,10 @@
 <h1>
     KANBAN BOARD
 </h1>
+<main class="w-full p-8 bg-gray-100 h-[400px] flex justify-between items-center">
+        <section class="h-[350px] w-[100px] bg-white border-2 border-black flex flex-col items-center justify-center">
+        </section>
+    <section  class="h-[350px] w-[100px] bg-white border-2 border-black flex flex-col items-center justify-center">
+    </section>
+    
+</main>
